1ad793ca25822dc0107afc1898da9e1a93aee8f8
Maria <marysiahalamska@gmail,com>
2026-04-17 11:01:49 +0200

    dark mode! v1

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("I'm working on it");
+const toggles = document.querySelectorAll("#toggleLD, #toggleLDmobile");
+
+function setTheme(isDark) {
+  document.documentElement.classList.toggle("dark", isDark);
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+}
+
+function toggleTheme() {
+  const isDark = document.documentElement.classList.contains("dark");
+  setTheme(!isDark);
+}
+
+// kliknięcia
+toggles.forEach((btn) => {
+  if (btn) btn.addEventListener("click", toggleTheme);
+});
+
+// start strony
+document.addEventListener("DOMContentLoaded", () => {
+  const saved = localStorage.getItem("theme");
+
+  if (saved) {
+    setTheme(saved === "dark");
+  } else {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+    setTheme(prefersDark);
+  }
+});
